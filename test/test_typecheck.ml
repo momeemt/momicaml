c348da5c1086e59e02f917453759fd321bc10e01
Mutsuha Asada <me@momee.mt>
2024-02-03 16:34:51 +0900

    added test for test_typecheck

--- test/test_typecheck.ml
+++ test/test_typecheck.ml
@@ -1,33 +1,88 @@
 open Momicaml.Syntax
 open Momicaml.Typecheck
+open Momicaml.Environment
 
 let test_typecheck_successful () =
   let expr = If (BoolLit true, IntLit 1, IntLit 100) in
-  match tcheck expr with
+  match tcheck (Environment.emptyEnv()) expr with
   | Ok TInt -> Alcotest.(check bool) "typecheck_successful" true true
   | _ -> Alcotest.fail "typecheck failed"
 
 let test_typecheck_failed () =
   let expr = If (BoolLit true, Plus (IntLit 1, BoolLit false), IntLit 100) in
-  match tcheck expr with
+  match tcheck (Environment.emptyEnv()) expr with
   | Error e -> Alcotest.(check bool) e true true
   | _ -> Alcotest.fail "typecheck failed"
 
 let test_typecheck_eq_int () =
   let expr = Eq (IntLit 1, IntLit 2) in
-  match tcheck expr with
+  match tcheck (Environment.emptyEnv()) expr with
   | Ok TBool -> Alcotest.(check bool) "typecheck_eq_int" true true
   | _ -> Alcotest.fail "typecheck failed"
 
 let test_typecheck_eq_bool () =
   let expr = Eq (BoolLit true, BoolLit false) in
-  match tcheck expr with
+  match tcheck (Environment.emptyEnv()) expr with
   | Ok TBool -> Alcotest.(check bool) "typecheck_eq_bool" true true
   | _ -> Alcotest.fail "typecheck failed"
 
 let test_typecheck_eq_failed () =
   let expr = Eq (IntLit 0, BoolLit false) in
-  match tcheck expr with
+  match tcheck (Environment.emptyEnv()) expr with
+  | Error e -> Alcotest.(check bool) e true true
+  | _ -> Alcotest.fail "typecheck failed"
+
+let test_typecheck_env1 () =
+  let expr = If (BoolLit true, Var "x", IntLit 100) in
+  let env =
+    Environment.ext
+    (Environment.ext
+       (Environment.emptyEnv()) "y" TInt)
+    "x" TInt
+  in
+  match tcheck env expr with
+  | Ok TInt -> Alcotest.(check bool) "typecheck_env1" true true
+  | _ -> Alcotest.fail "typecheck failed"
+
+let test_typecheck_env2 () =
+  let expr = If (BoolLit true, Var "x", IntLit 100) in
+  let env =
+    Environment.ext
+    (Environment.ext
+       (Environment.emptyEnv()) "y" TInt)
+    "x" TBool
+  in
+  match tcheck env expr with
+  | Error e -> Alcotest.(check bool) e true true
+  | _ -> Alcotest.fail "typecheck failed"
+
+let test_typecheck_env3 () =
+  let expr = If (BoolLit true, Var "x", IntLit 100) in
+  let env =
+    Environment.ext
+    (Environment.ext
+       (Environment.emptyEnv()) "y" TInt)
+    "z" TInt
+  in
+  match tcheck env expr with
+  | Error e -> Alcotest.(check bool) e true true
+  | _ -> Alcotest.fail "typecheck failed"
+
+let test_typecheck_env4 () =
+  let expr = If (Var "x", Var "x", IntLit 100) in
+  let env =
+    Environment.ext (Environment.emptyEnv()) "x" TBool
+  in
+  match tcheck env expr with
+  | Error e -> Alcotest.(check bool) e true true
+  | _ -> Alcotest.fail "typecheck failed"
+
+let test_typecheck_env5 () =
+  let expr = If (Var "x", Var "x", IntLit 100) in
+  let env =
+    Environment.ext (Environment.emptyEnv()) "x" TInt
+  in
+  match tcheck env expr with
   | Error e -> Alcotest.(check bool) e true true
   | _ -> Alcotest.fail "typecheck failed"
 
@@ -46,5 +101,15 @@ let () =
             test_typecheck_eq_bool;
           Alcotest.test_case "test_typecheck_eq_failed" `Quick
             test_typecheck_failed;
+          Alcotest.test_case "test_typecheck_env1" `Quick
+            test_typecheck_env1;
+          Alcotest.test_case "test_typecheck_env2" `Quick
+            test_typecheck_env2;
+          Alcotest.test_case "test_typecheck_env3" `Quick
+            test_typecheck_env3;
+          Alcotest.test_case "test_typecheck_env4" `Quick
+            test_typecheck_env4;
+          Alcotest.test_case "test_typecheck_env5" `Quick
+            test_typecheck_env5;
         ] );
     ]
