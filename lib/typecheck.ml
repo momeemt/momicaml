694fbc37d8d7cde49636ff381264bc6fdbc9e527
Mutsuha Asada <me@momee.mt>
2024-02-03 16:34:39 +0900

    added typecheck for variables

--- lib/typecheck.ml
+++ lib/typecheck.ml
@@ -1,24 +1,25 @@
 open Syntax
 open Result
+open! Environment
 
 type ty = TInt | TBool
+type tyenv = (string, ty) Hashtbl.t
 
-let string_of_ty t = match t with TInt -> "int" | TBool -> "bool"
-
-let rec tcheck e =
+let rec tcheck te e =
   match e with
+  | Var s -> Environment.lookup s te
   | IntLit _ -> ok TInt
   | BoolLit _ -> ok TBool
   | Plus (e1, e2) -> (
-      match (tcheck e1, tcheck e2) with
+      match (tcheck te e1, tcheck te e2) with
       | Ok TInt, Ok TInt -> ok TInt
       | _ -> error "type error in Plus")
   | Eq (e1, e2) -> (
-      match (tcheck e1, tcheck e2) with
+      match (tcheck te e1, tcheck te e2) with
       | Ok t1, Ok t2 -> if t1 = t2 then ok TBool else error "type error in Eq"
       | _ -> error "type error in Eq")
   | If (e1, e2, e3) -> (
-      match (tcheck e1, tcheck e2, tcheck e3) with
+      match (tcheck te e1, tcheck te e2, tcheck te e3) with
       | Ok TBool, Ok t2, Ok t3 ->
           if t2 = t3 then ok t2 else error "type error in If"
       | _ -> error "type error in If")
