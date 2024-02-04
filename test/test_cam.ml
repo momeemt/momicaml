d0caf0e59159770dea1af0966ed1c5b70aed31a7
Mutsuha Asada <me@momee.mt>
2024-02-04 18:34:48 +0900

    ocamlformat

--- test/test_cam.ml
+++ test/test_cam.ml
@@ -1,40 +1,39 @@
 open Momicaml.Cam
 
 let test_cam1 () =
-  let cam = [
-    CAM.CAM_Closure [
-      CAM.CAM_Ldi 1;
-      CAM.CAM_Access 0;
-      CAM.CAM_Eq;
-      CAM.CAM_Test (
+  let cam =
+    [
+      CAM.CAM_Closure
         [
-          CAM.CAM_Ldi 1
-        ],[
-          CAM.CAM_Ldi (-1);
+          CAM.CAM_Ldi 1;
           CAM.CAM_Access 0;
-          CAM.CAM_Add;
-          CAM.CAM_Access 1;
-          CAM.CAM_Apply;
-          CAM.CAM_Access 0;
-          CAM.CAM_Add;
-        ]
-      );
-      CAM.CAM_Return
-    ];
-    CAM.CAM_Let;
-    CAM.CAM_Ldi 10;
-    CAM.CAM_Access 0;
-    CAM.CAM_Apply;
-    CAM.CAM_EndLet;
-  ]
-  in 
-  let stack = CAM.eval cam [] [] in 
+          CAM.CAM_Eq;
+          CAM.CAM_Test
+            ( [ CAM.CAM_Ldi 1 ],
+              [
+                CAM.CAM_Ldi (-1);
+                CAM.CAM_Access 0;
+                CAM.CAM_Add;
+                CAM.CAM_Access 1;
+                CAM.CAM_Apply;
+                CAM.CAM_Access 0;
+                CAM.CAM_Add;
+              ] );
+          CAM.CAM_Return;
+        ];
+      CAM.CAM_Let;
+      CAM.CAM_Ldi 10;
+      CAM.CAM_Access 0;
+      CAM.CAM_Apply;
+      CAM.CAM_EndLet;
+    ]
+  in
+  let stack = CAM.eval cam [] [] in
   let res = List.hd stack in
   match res with
   | CAM.CAM_IntCal i -> Alcotest.(check int) "test_cam1" 55 i
   | _ -> Alcotest.fail "test_cam1"
 
 let () =
-  Alcotest.run "Momicaml" [
-    "test_cam1", [Alcotest.test_case "test_cam1" `Quick test_cam1];
-  ]
+  Alcotest.run "Momicaml"
+    [ ("test_cam1", [ Alcotest.test_case "test_cam1" `Quick test_cam1 ]) ]
