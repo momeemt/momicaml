f4953bbdb9431e8fc35b4b0c7de7ea44f0674da7
Mutsuha Asada <me@momee.mt>
2024-02-04 14:34:34 +0900

    added tests for tcheck

--- test/test_typecheck.ml
+++ test/test_typecheck.ml
@@ -2,6 +2,11 @@ open Momicaml.Syntax
 open Momicaml.Typecheck
 open Momicaml.Environment
 
+let rec type_to_string = function
+  | TInt -> "int"
+  | TBool -> "bool"
+  | TArrow (t1, t2) -> Printf.sprintf "(%s -> %s)" (type_to_string t1) (type_to_string t2)
+
 let test_typecheck_successful () =
   let expr = If (BoolLit true, IntLit 1, IntLit 100) in
   match tcheck (Environment.emptyEnv()) expr with
@@ -86,6 +91,66 @@ let test_typecheck_env5 () =
   | Error e -> Alcotest.(check bool) e true true
   | _ -> Alcotest.fail "typecheck failed"
 
+let test_typecheck_fn1 () =
+  let expr = Fun ("x", If (BoolLit true, Var "x", IntLit 100)) in
+  let env = Environment.ext (Environment.emptyEnv()) "x" TInt in
+  match tcheck env expr with
+  | Ok (TArrow (TInt, TInt)) -> Alcotest.(check bool) "typecheck_fn1" true true
+  | _ -> Alcotest.fail "typecheck failed"
+
+let test_typecheck_fn2 () =
+  let expr = Fun ("x", If (BoolLit true, Var "x", IntLit 100)) in
+  let env = Environment.ext (Environment.emptyEnv()) "x" TBool in
+  match tcheck env expr with
+  | Error e -> Alcotest.(check bool) e true true
+  | _ -> Alcotest.fail "typecheck failed"
+
+let test_typecheck_fn3 () =
+  let expr =
+    App
+      ( Fun ("x", If (BoolLit true, Var "x", IntLit 100)),
+        If (BoolLit true, Var "y", IntLit 200) ) in
+  let env = Environment.ext (Environment.ext (Environment.emptyEnv()) "x" TInt) "y" TInt in
+  match tcheck env expr with
+  | Ok TInt -> Alcotest.(check bool) "typecheck_fn3" true true
+  | _ -> Alcotest.fail "typecheck failed"
+
+let test_typecheck_fn4 () =
+  let expr =
+    Fun
+      ( "f",
+        Fun
+          ( "x",
+            App
+              ( Var "f",
+                App
+                  ( Var "f",
+                    App
+                      ( Var "f",
+                        Plus (Var "x", IntLit 10) ) ) ) ) ) in
+  let env = Environment.ext (Environment.ext (Environment.emptyEnv()) "x" TInt) "f" (TArrow (TInt, TInt)) in
+  match tcheck env expr with
+  | Ok (TArrow (TArrow (TInt, TInt), TArrow (TInt, TInt))) -> Alcotest.(check bool) "typecheck_fn4" true true
+  | _ -> Alcotest.fail "typecheck failed"
+
+let test_typecheck_fn5 () =
+  let expr =
+    Fun
+      ( "f",
+        Fun
+          ( "g",
+            Fun
+              ( "x",
+                App
+                  ( Var "f",
+                    App
+                      ( Var "g",
+                        Var "x" ) ) ) ) ) in
+  let env = Environment.ext (Environment.ext (Environment.ext (Environment.emptyEnv()) "x" TInt) "g" (TArrow (TInt, TInt))) "f" (TArrow (TInt, TInt)) in
+  match tcheck env expr with
+  | Ok (TArrow (TArrow (TInt, TInt), TArrow(TArrow (TInt, TInt), TArrow (TInt, TInt)))) -> Alcotest.(check bool) "typecheck_fn5" true true
+  | _ -> Alcotest.fail "typecheck failed"
+
 let () =
   Alcotest.run "Momicaml.Typecheck"
     [
@@ -111,5 +176,15 @@ let () =
             test_typecheck_env4;
           Alcotest.test_case "test_typecheck_env5" `Quick
             test_typecheck_env5;
+          Alcotest.test_case "test_typecheck_fn1" `Quick
+            test_typecheck_fn1;
+          Alcotest.test_case "test_typecheck_fn2" `Quick
+            test_typecheck_fn2;
+          Alcotest.test_case "test_typecheck_fn3" `Quick
+            test_typecheck_fn3;
+          Alcotest.test_case "test_typecheck_fn4" `Quick
+            test_typecheck_fn4;
+          Alcotest.test_case "test_typecheck_fn5" `Quick
+            test_typecheck_fn5;
         ] );
     ]
