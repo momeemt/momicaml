68b5cd72abe2f76035ff4f248e3bc4f4476c49a8
Mutsuha Asada <me@momee.mt>
2024-02-05 18:17:23 +0900

    implemented list

--- lib/eval.ml
+++ lib/eval.ml
@@ -2,6 +2,16 @@ open Syntax
 open! Environment
 
 module Eval = struct
+  let rec equal_vals v1 v2 = match (v1, v2) with
+    | IntVal n1, IntVal n2 -> n1 = n2
+    | BoolVal b1, BoolVal b2 -> b1 = b2
+    | ListVal l1, ListVal l2 -> equal_lists l1 l2
+    | _, _ -> failwith "type error"
+  and equal_lists l1 l2 = match (l1, l2) with
+    | [], [] -> true
+    | h1::t1, h2::t2 -> equal_vals h1 h2 && equal_lists t1 t2
+    | _, _ -> false
+
   let rec eval e env =
     let binop f e1 e2 env =
       let right = eval e2 env in
@@ -19,10 +29,8 @@ module Eval = struct
     | Eq (e1, e2) -> (
         let right = eval e2 env in
         let left = eval e1 env in
-        match (left, right) with
-        | IntVal n1, IntVal n2 -> BoolVal (n1 = n2)
-        | BoolVal b1, BoolVal b2 -> BoolVal (b1 = b2)
-        | _, _ -> failwith "type error")
+        BoolVal (equal_vals left right)
+    )
     | Neq (e1, e2) -> (
         let right = eval e2 env in
         let left = eval e1 env in
@@ -64,5 +72,20 @@ module Eval = struct
             let env2 = Environment.ext (Environment.ext env1 x arg) f funpart in
             eval body env2
         | _ -> failwith "function value expected")
+    | Empty -> ListVal []
+    | Cons (e1, e2) -> (
+        let head = eval e1 env in
+        let tail = eval e2 env in
+        match (head, tail) with
+        | v1, ListVal v2 -> ListVal (v1 :: v2)
+        | _, _ -> failwith "type error")
+    | Head e -> (
+        match eval e env with
+        | ListVal (h :: _) -> h
+        | _ -> failwith "type error")
+    | Tail e -> (
+        match eval e env with
+        | ListVal (_ :: t) -> ListVal t
+        | _ -> failwith "type error")
     | _ -> failwith "not implemented"
 end
