950b19c6b9f8df5c0e613d8338efbeccb93b799e
Mutsuha Asada <me@momee.mt>
2024-02-14 18:59:33 +0900

    added simple constant propagation

--- lib/codegen.ml
+++ lib/codegen.ml
@@ -1,18 +1,27 @@
 open Syntax
 open Cam
 open Result
+open Option
+open List
 
 module Codegen = struct
   let rec position x venv =
     match venv with
     | [] -> failwith "no matching variable in environment"
-    | y :: venv2 -> if x = y then 0 else 1 + position x venv2
+    | (y, _) :: venv2 -> if x = y then 0 else 1 + position x venv2
 
   let rec cam_codegen exp venv =
     match exp with
-    | Ok (Var x) -> ok [ CAM.CAM_Access (position x venv) ]
+    | Ok (Var x) -> (
+        let pos = position x venv in
+        let (_, value) = List.nth venv pos in
+        match value with
+        | Some (CAM.CAM_Ldi i) -> ok [ CAM.CAM_Ldi i ]
+        | Some (CAM.CAM_Ldb b) -> ok [ CAM.CAM_Ldb b ]
+        | Some _ | None -> ok [ CAM.CAM_Access pos ]
+    )
     | Ok (Fun (x, e)) -> (
-        let res = cam_codegen (ok e) (x :: "_" :: venv) in
+        let res = cam_codegen (ok e) ((x, none) :: ("_", none) :: venv) in
         match res with
         | Ok code -> ok [ CAM.CAM_Closure (code @ [ CAM.CAM_Return ]) ]
         | Error e -> Error e)
@@ -29,17 +38,21 @@ module Codegen = struct
         let res1 = cam_codegen (ok e1) venv in
         match res1 with
         | Ok code1 -> (
-            let res2 = cam_codegen (ok e2) (x :: venv) in
+            let res2 = if List.length code1 = 1 then
+                cam_codegen (ok e2) ((x, List.nth code1 0 |> some) :: venv)
+            else
+                cam_codegen (ok e2) ((x, none) :: venv)
+            in
             match res2 with
             | Ok code2 ->
                 ok (code1 @ [ CAM.CAM_Let ] @ code2 @ [ CAM.CAM_EndLet ])
             | Error e -> Error e)
         | Error e -> Error e)
     | Ok (LetRec (f, x, e1, e2)) -> (
-        let res1 = cam_codegen (ok e1) (x :: f :: venv) in
+        let res1 = cam_codegen (ok e1) ((x, none) :: (f, none) :: venv) in
         match res1 with
         | Ok code1 -> (
-            let res2 = cam_codegen (ok e2) (f :: venv) in
+            let res2 = cam_codegen (ok e2) ((f, none) :: venv) in
             match res2 with
             | Ok code2 ->
                 ok
